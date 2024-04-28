71b46d670b6906171f26e3393cab13f5e002a1b9
Mostafa Tehrani <mostafa.tehrani.dev@gmail.com>
2024-04-28 17:19:12 +0300

    add qutebrowser config

---
+++ qutebrowser/.config/qutebrowser/shortcuts.py
@@ -0,0 +1,30 @@
+config.bind(
+    ";dc", "set downloads.location.directory ~/Documents ;; hint links download"
+)
+config.bind(
+    ";dl", "set downloads.location.directory ~/Downloads ;; hint links download"
+)
+config.bind(
+    ";yt",
+    "set downloads.location.directory ~/Downloads/Video/youtube-dl ;; hint links download",
+)
+config.bind(";m", "set downloads.location.directory ~/Music ;; hint links download")
+config.bind(
+    ";W",
+    "set downloads.location.directory ~/Pictures/backgrounds ;; hint links download",
+)
+config.bind(
+    ";V", "set downloads.location.directory ~/Downloads/Video ;; hint links download"
+)
+config.bind(";S", "set downloads.location.directory ~/.scripts ;; hint links download")
+config.bind(";cf", "set downloads.location.directory ~/.config ;; hint links download")
+config.bind(
+    ";T",
+    "set downloads.location.directory ~/programing/toturials ;; hint links download",
+)
+config.bind(";pr", "set downloads.location.directory ~/projects ;; hint links download")
+config.bind(
+    ";D",
+    "set downloads.location.directory ~/.config/suckless/dwm-f ;; hint links download",
+)
+config.bind(";a", "set downloads.location.directory ~/mobail ;; hint links download")
